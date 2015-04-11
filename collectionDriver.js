cf0ac7a14a8eb7f9dbf71b9cfcfc7195cf5da7dd
Shakeeb Saleh <Shakeeb8644@gmail.com>
2015-04-11 09:07:43 -0400

    penis

--- collectionDriver.js
+++ collectionDriver.js
@@ -15,7 +15,7 @@ CollectionDriver.prototype.getCollection = function(collectionName, callback) {
 CollectionDriver.prototype.findAll = function(collectionName, callback) {
     this.getCollection(collectionName, function(error, the_collection) { //A
       if( error ) callback(error)
-      else {
+      else { // function is anonymous
           the_collection.find().sort({'Score' : -1}).toArray(function(error, results) { //B
             if( error ) callback(error)
             else {
@@ -55,10 +55,62 @@ CollectionDriver.prototype.save = function(collectionName, obj, callback) {
     });
 };
 
+// what we need to do is get the json from the request object, store the json into our db
+// parse the json, iterate across every other json in our db
+// if any of the jsons are too close, less than a certain threshold,
+// place certain things in certain palces
+
+CollectionDriver.prototype.calculateDistance = function(obj1, obj2){ 
+  // x1y1 has to be the larger number
+  // var x1, x2, y1, y2 
+  var x1 = obj1.location.x;
+  var x2 = obj2.location.x;
+  var y1 = obj1.location.y;
+  var y2 = obj2.location.y;
+  // grbs ll coordinates
+  var x = x1-x2;
+  var y = y1-y2;
+  x = Math.pow(x, 2);
+  y = Math.pow(y, 2);
+  var result = x+y;
+  var result = Math.pow(result, (1/2));
+  return result;
+}
+
+CollectonDriver.prototype.fullCalculate = function(collectionName, obj, callback){
+  // ths fucntion will clculate across all of the JSON's tht we have, treat them all as objects
+  /// returns the distance, whos colliding as well as true or false k
+  // everything is always done withn the callback
+  var length;
+  var i;
+  var collection = getCollection(collectionName, function(error, collection){
+    if(error){ callBack(error, false);
+    } else () {
+      i = collection.getLength();
+      for (i; i!=0 ;i--){
+    // iterate through, getting every object from the collection,
+    // and passing it in to the distance function
+      var comparator = collection.toArray[i]; 
+        if(comparator.name =! obj.name){
+          length = calculateDistance(comparator, obj);
+          if(length < 1000){ // some arbitrary number 
+            //return comparator; idk how to return ll these details
+            // so imma assume that the code that clled me saves the json that is obj1
+            //  
+            callback(comparator, true); // true case
+          }
+      }
+  } //return null; //case if it's false, nothing else matters
+  callback(null, false); // false
+} 
+})
+
+}
+
 //update a specific object
 CollectionDriver.prototype.update = function(collectionName, obj, entityId, callback) {
     this.getCollection(collectionName, function(error, the_collection) {
-        if (error) callback(error)
+        if (error) callback(error) // fuck idont undertand what callback is but whatever
         else {
 	        obj._id = ObjectID(entityId); //A convert to a real obj id
 	        obj.updated_at = new Date(); //B
--- index.js
+++ index.js
@@ -4,16 +4,19 @@ var http = require('http'),
     MongoClient = require('mongodb').MongoClient,
     Server = require('mongodb').Server,
     CollectionDriver = require('./collectionDriver').CollectionDriver;
- 
+
+// metadata  
 var app = express();
 app.set('port', process.env.PORT || 8080); 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(express.bodyParser()); // <-- add
 
+// 
 var mongoHost = 'localHost'; //A
 var mongoPort = 27017; 
 var collectionDriver;
+var JSONtemp;
  
 var mongoClient = new MongoClient(new Server(mongoHost, mongoPort)); //B
 mongoClient.open(function(err, mongoClient) { //C
@@ -25,16 +28,18 @@ mongoClient.open(function(err, mongoClient) { //C
   collectionDriver = new CollectionDriver(db); //F
 });
 
+
 app.use(express.static(path.join(__dirname, 'public')));
  
 app.get('/', function (req, res) {
   res.render('main')
 });
  
-
+// ths is our get function, this s importnat
+// 
 app.get('/data', function(req, res) { //A
-   var params = req.params; //B
-   dbCollection = 'players'
+   var params = req.params; //B this gets al params
+   dbCollection = 'players' // this makes a collection
    //console.log(req)
    collectionDriver.findAll(dbCollection, function(error, objs) { //C
         if (error) { res.send(400, error); } //D
@@ -49,11 +54,17 @@ app.get('/data', function(req, res) { //A
     });
 });
 
+// we fuck with post
+// in post, the body, is teh json, sooooo.... 
+//we place the json in the db, as well as comparing it to every other thing in the db
+// and then i worry about returning, later
 app.post('/data', function(req, res) { //A
-    var object = req.body;
-    console.log(object)
+    var object = req.body; // this is the JSON
+    console.log(object)// this is theest that shows tht the post went through
     var dbCollection = 'players';
-    object.Score = parseInt(object.Score)
+    JSONtemp = object;
+    // insted of object.score, we'll implement object.x or object.y, probbly both
+    object.Score = parseInt(object.Score) // idk why this is done tho
     collectionDriver.save(dbCollection, object, function(err,docs) {
           if (err) { res.send(400, err); } 
           else { res.send(201, docs); } //B
